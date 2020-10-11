9d3ae4cd46342ff47479e6a1ef2ee8221dd6c119
davaponte <david.aponte@outlook.com>
2020-10-11 13:18:42 -0500

    Versión comprimida
    La anterior fue mal subida

--- works/snake/Snake.js
+++ works/snake/Snake.js
@@ -28,7 +28,7 @@ var Preloader = /** @constructor */ function() {
 	var loadingFiles = {};
 	this.preloadedFiles = [];
 
-	function loadXHR(resolve, reject, file, tracker) {
+	    function loadXHR(resolve, reject, file, tracker) {  if (file.substr(-5) === '.wasm' || file.substr(-4) === '.pck') { file += '.gz'; var resolve_orig = resolve; resolve = function(xhr) { return resolve_orig(xhr.responseURL.substr(-3) === '.gz' ? { response: pako.inflate(xhr.response),    responseType: xhr.responseType, responseURL: xhr.responseURL, status: xhr.status,   statusText: xhr.statusText } : xhr); }; }
 		var xhr = new XMLHttpRequest;
 		xhr.open('GET', file);
 		if (!file.endsWith('.js')) {
